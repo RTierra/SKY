97c7c7afa14f27d96f318b383f72ccbdc749f70d
Ricardo Tierra <ricardo.anthony.tierra@Gmail.com>
2019-09-14 00:05:58 -0500

    Primera version de SKY

---
+++ src/routes/notes.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+// Models
+const Note = require('../models/Note');
+
+// Helpers
+const { isAuthenticated } = require('../helpers/auth');
+
+// New Note
+router.get('/notes/add', isAuthenticated, (req, res) => {
+  res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', isAuthenticated, async (req, res) => {
+  const { title, description } = req.body;
+  const errors = [];
+  if (!title) {
+    errors.push({text: 'Ingresa un titulo.'});
+  }
+  if (!description) {
+    errors.push({text: 'Ingresa tu descripción'});
+  }
+  if (errors.length > 0) {
+    res.render('notes/new-note', {
+      errors,
+      title,
+      description
+    });
+  } else {
+    const newNote = new Note({title, description});
+    newNote.user = req.user.id;
+    await newNote.save();
+    req.flash('success_msg', 'Nota agregada');
+    res.redirect('/notes');
+  }
+});
+
+// Get All Notes
+router.get('/notes', isAuthenticated, async (req, res) => {
+  const notes = await Note.find({user: req.user.id}).sort({date: 'desc'});
+  res.render('notes/all-notes', { notes });
+});
+
+// Edit Notes
+router.get('/notes/edit/:id', isAuthenticated, async (req, res) => {
+  const note = await Note.findById(req.params.id);
+  if(note.user != req.user.id) {
+    req.flash('error_msg', 'Sin autorización');
+    return res.redirect('/notes');
+  } 
+  res.render('notes/edit-note', { note });
+});
+
+router.put('/notes/edit-note/:id', isAuthenticated, async (req, res) => {
+  const { title, description } = req.body;
+  await Note.findByIdAndUpdate(req.params.id, {title, description});
+  req.flash('success_msg', 'Nota editada');
+  res.redirect('/notes');
+});
+
+// Delete Notes
+router.delete('/notes/delete/:id', isAuthenticated, async (req, res) => {
+  await Note.findByIdAndDelete(req.params.id);
+  req.flash('success_msg', 'Nota borrada');
+  res.redirect('/notes');
+});
+
+module.exports = router;
---
+++ src/routes/users.js
@@ -0,0 +1,58 @@
+const router = require('express').Router();
+const passport = require('passport');
+
+// Models
+const User = require('../models/User');
+
+router.get('/users/signup', (req, res) => {
+  res.render('users/signup');
+});
+
+router.post('/users/signup', async (req, res) => {
+  let errors = [];
+  const { name, email, password, confirm_password } = req.body;
+  if(name.length < 4) {
+    errors.push({text: 'El usuario debe tener mas de 4 caracteres.'});
+  }
+  if(password != confirm_password) {
+    errors.push({text: 'Las contraseñas no coinciden.'});
+  }
+  if(password.length < 4) {
+    errors.push({text: 'Ingresa una contraseña mayor a 4 caracteres.'})
+  }
+  if(errors.length > 0){
+    res.render('users/signup', {errors, name, email, password, confirm_password});
+  } else {
+    // Look for email coincidence
+    const emailUser = await User.findOne({email: email});
+    if(emailUser) {
+      req.flash('error_msg', 'Este email ya esta en uso.');
+      res.redirect('/users/signup');
+    } else {
+      // Saving a New User
+      const newUser = new User({name, email, password});
+      newUser.password = await newUser.encryptPassword(password);
+      await newUser.save();
+      req.flash('success_msg', 'Estas registrado.');
+      res.redirect('/users/signin');
+    }
+  }
+});
+
+router.get('/users/signin', (req, res) => {
+  res.render('users/signin');
+});
+
+router.post('/users/signin', passport.authenticate('local', {
+  successRedirect: '/notes',
+  failureRedirect: '/users/signin',
+  failureFlash: true
+}));
+
+router.get('/users/logout', (req, res) => {
+  req.logout();
+  req.flash('success_msg', 'Has cerrado tu sessión.');
+  res.redirect('/users/signin');
+});
+
+module.exports = router;
